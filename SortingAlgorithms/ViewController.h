48deab881eb4df07d9eff4a597ed43a97a024749
Kamil Kwiatkowski <kamil.kwiatkowski@tomtom.com>
2018-09-19 08:45:46 +0200

    Initial commit

---
+++ SortingAlgorithms/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SortingAlgorithms
+//
+//  Created by Kamil Kwiatkowski on 19/09/2018.
+//  Copyright © 2018 Kamil Kwiatkowski. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ SortingAlgorithms/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SortingAlgorithms
+//
+//  Created by Kamil Kwiatkowski on 19/09/2018.
+//  Copyright © 2018 Kamil Kwiatkowski. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
